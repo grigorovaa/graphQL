caf0473d33b8195d325b906b1b112b97056bd968
grigorovaa <megrimarik13@gmail.com>
2024-02-03 22:42:43 +0200

    define a query, add useQuery hook

--- client/codegen.ts
+++ client/codegen.ts
@@ -1,1 +1,16 @@
-// TODO
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  schema: "http://localhost:4000",
+  documents: ["src/**/*.tsx"],
+  generates: {
+    "./src/__generated__/": {
+      preset: "client",
+      presetConfig: {
+        gqlTagName: "gql",
+      },
+    },
+  },
+};
+
+export default config;
